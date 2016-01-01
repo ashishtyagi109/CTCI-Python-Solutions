a479c3d82dea84cd0ec25a6c0d7f3f3c04dca2ce
ashishtyagi109 <ashishty@buffalo.edu>
2016-01-01 22:15:16 +1100

    Updated code to get input value from user and find its successor

--- Question_4_6.py
+++ Question_4_6.py
@@ -1,3 +1,5 @@
+from Generate_BST_With_Input import *
+
 def successor(node):
 	if node._right is not None:
 		return leftMost(node._right)
@@ -15,3 +17,27 @@ def checkParent(parent, node):
 		return parent
 	else:
 		return checkParent(parent._parent, parent)
+
+def findInTree(root, value):
+	while root is not None:
+		if value < root._value:
+			root = root._left
+		elif value > root._value:
+			root = root._right
+		else:
+			break # value = root._value
+	return root
+
+if __name__ == '__main__':
+	root = generateBST()
+
+	input = int(raw_input('Enter value of node whose successor is required: '))
+	node = findInTree(root, input)
+	if node is None:
+		print 'Value not found in tree'
+	else:
+		successor = successor(node)
+		if successor is None:
+			print 'No successor for input node'
+		else:
+			print 'successor is: ', successor
