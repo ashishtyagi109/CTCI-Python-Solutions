a82fa5a2c830a89e55222cdb1cbc6aa3efacd4ac
ashishtyagi109 <ashishty@buffalo.edu>
2016-01-08 10:21:43 +1100

    Added reverseList and isEqual methods

--- LinkedList_Helpers.py
+++ LinkedList_Helpers.py
@@ -49,3 +49,20 @@ def getList(head):
 		linkedList.append(head.value)
 		head = head.next
 	return linkedList
+
+# Returns singly linked list
+def reverseList(head):
+	rHead = None
+	while head is not None:
+		temp = Node(head.value, rHead, None)
+		rHead = temp
+		head = head.next
+	return rHead
+
+def isEqual(head1, head2):
+	while head1 is not None and head2 is not None:
+		if head1.value != head2.value:
+			return False
+		head1 = head1.next
+		head2 = head2.next
+	return head1 is None and head2 is None
