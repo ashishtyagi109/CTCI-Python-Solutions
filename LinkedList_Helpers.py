f40aeb749b5107fd5dab06e0b5bf9b94ea4ac257
ashishtyagi109 <ashishty@buffalo.edu>
2016-01-08 12:47:58 +1100

    Added getKthNode method

--- LinkedList_Helpers.py
+++ LinkedList_Helpers.py
@@ -66,3 +66,11 @@ def isEqual(head1, head2):
 		head1 = head1.next
 		head2 = head2.next
 	return head1 is None and head2 is None
+
+def getKthNode(head, k):
+	while k > 0 and head is not None:
+		k -= 1
+		head = head.next
+
+	return head
+	
